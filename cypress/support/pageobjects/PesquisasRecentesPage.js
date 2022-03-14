f2cfd81d019a9ee2a07c06edc4dd0cc106e20aef
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2022-03-14 09:56:45 -0300

    Adicionando .wait para verificar com mais visibilidade as operações que estão sendo executadas

--- cypress/support/pageobjects/PesquisasRecentesPage.js
+++ cypress/support/pageobjects/PesquisasRecentesPage.js
@@ -5,14 +5,17 @@ require('cypress-xpath')
 
 class PesquisasRecentesPage {
     acessarSite() {
+        cy.wait(1000)
         cy.visit(url)
     }
 
     abreAsConfigurações() {
+        cy.wait(1000)
         cy.xpath(pesquisasRecentesElements.botaoConfig()).click()
     }
 
     editaAsFlagsParaFalse() {
+        cy.wait(1000)
         cy.xpath(pesquisasRecentesElements.botaoEditarUseBlufftonLayout()).click()
         cy.xpath(pesquisasRecentesElements.inputUseBlufftonLayour()).clear().type('false{enter}')
         cy.xpath(pesquisasRecentesElements.botaoEditarHideRecentSearch()).click()
@@ -21,22 +24,27 @@ class PesquisasRecentesPage {
     }
 
     verificaLimpaInput() {
+        cy.wait(1000)
         cy.xpath(pesquisasRecentesElements.inputOrdemProducaoMaterial()).clear()
     }
 
     inserirIdValido() {
+        cy.wait(1000)
         cy.xpath(pesquisasRecentesElements.inputOrdemProducaoMaterial()).type('12000240{enter}')
     }
 
     selecionaOPrimeiroDocumento() {
+        cy.wait(1000)
         cy.xpath(pesquisasRecentesElements.primeiraOpcaoDocumento()).click()
     }
 
     voltaParaHome() {
+        cy.wait(1000)
         cy.xpath(pesquisasRecentesElements.botaoHome()).click()
     }
 
     secionaPrimeiraOpcaoRecente() {
+        cy.wait(1000)
         cy.xpath(pesquisasRecentesElements.primeiraOpcaoRecentSearchs()).click()
     }
 
